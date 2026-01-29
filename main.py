b4ddbc4d2236d4cf0d0c3063c29df522f02ac975
Andrea Ochoa Córdova <91387374+AndreaOchoaC@users.noreply.github.com>
2026-01-29 16:51:54 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+# Este es el archivo principal de nuestro proyecto
+# Objetivo: Crear un bot de recomendaciones
+
+print("¿Qué quieres que te recomiende hoy?")
+ans = int(input("1. Películas \n 2. Series \n 3. Videojuegos \n"))
+
+# Tarea 1 - Leo: Crear lista de películas
+lista_peliculas = ()
+
+# Tarea 2 - Mati: Crear lista de series
+lista_series = ()
+
+# Tarea 3 - Naiyah: Crear lista de videojuegos
+lista_videojuegos = ()
+
+for i in range(0, len(lista_series)):
+    opcion_series = lista_series[i]
+for i in range(0, len(lista_peliculas)):
+    opcion_pelis = lista_peliculas[i]
+for i in range(0, len(lista_videojuegos)):
+    opcion_videos = lista_videojuegos[i]
+
+if ans == 1:
+    print("Te recomiendo la película: ")
+elif ans == 2:
+    print("Te recomiendo la serie: ")
+elif ans == 3:
+    print("Te recomiendo el videojuego: ")
+else:
+    print("Ingresa una opción válida.")
+
